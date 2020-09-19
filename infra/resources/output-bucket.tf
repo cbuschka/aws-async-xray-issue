519fcdd6bd2516c8839f1777bb0e7ad0eee0ecff
conni <cbuschka@gmail.com>
2020-09-19 22:22:58 +0200

    Disabled versioning as it is not needed.

--- infra/resources/output-bucket.tf
+++ infra/resources/output-bucket.tf
@@ -2,9 +2,6 @@ resource "aws_s3_bucket" "output" {
   bucket = "${var.scope}-output"
   acl = "private"
   force_destroy = true
-  versioning {
-    enabled = true
-  }
   tags = {
     scope = "${var.scope}"
   }
