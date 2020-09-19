21cb9e1406e80c4a63c85d03c12057f7923fb19a
conni <cbuschka@gmail.com>
2020-09-19 22:24:31 +0200

    Added automatic deletion of s3 files after a day.

--- infra/resources/output-bucket.tf
+++ infra/resources/output-bucket.tf
@@ -2,6 +2,13 @@ resource "aws_s3_bucket" "output" {
   bucket = "${var.scope}-output"
   acl = "private"
   force_destroy = true
+  lifecycle_rule {
+    id = "remove_after_1d"
+    enabled = true
+    expiration {
+      days = 1
+    }
+  }
   tags = {
     scope = "${var.scope}"
   }
